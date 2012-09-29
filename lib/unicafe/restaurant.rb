7ef601408211163962c615dc9eda4f935022abf2
Sami Saada <sami.saada@gmail.com>
2012-09-29 17:12:35 +0300

    refactor name_to_id to use list constant

--- lib/unicafe/restaurant.rb
+++ lib/unicafe/restaurant.rb
@@ -1,19 +1,17 @@
 module Unicafe
   class Restaurant
 
+    LIST_OF_RESTAURANTS = {
+      10 => "Chemicum",
+      11 => "Exactum"
+    }
+
     def self.find_by_name name
       self.find_by_id self.name_to_id(name)
     end
 
     def self.name_to_id name
-      case name
-      when "Chemicum"
-        10
-      when "Exactum"
-        11
-      else
-        raise NotFound
-      end
+      LIST_OF_RESTAURANTS.invert[name] || raise(NotFound)
     end
 
     class NotFound < Exception
