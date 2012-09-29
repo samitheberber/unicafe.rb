de2defd033d8ebfc142fa5b4be4646b7bb56961a
Sami Saada <sami.saada@gmail.com>
2012-09-29 17:04:27 +0300

    find id by name

--- lib/unicafe/restaurant.rb
+++ lib/unicafe/restaurant.rb
@@ -5,6 +5,17 @@ module Unicafe
       self.find_by_id self.name_to_id(name)
     end
 
+    def self.name_to_id name
+      case name
+      when "Chemicum"
+        10
+      when "Exactum"
+        11
+      else
+        raise NotFound
+      end
+    end
+
     class NotFound < Exception
     end
 
--- spec/unicafe/restaurant_spec.rb
+++ spec/unicafe/restaurant_spec.rb
@@ -17,4 +17,23 @@ describe Unicafe::Restaurant do
     expect{Unicafe::Restaurant.find_by_name(name)}.to raise_error(Unicafe::Restaurant::NotFound)
   end
 
+  describe "#name_to_id" do
+
+    let(:exactum) { {id: 11, name: "Exactum"} }
+    let(:chemicum) { {id: 10, name: "Chemicum"} }
+
+    it "should give correct id for Exactum" do
+      Unicafe::Restaurant.name_to_id(exactum[:name]).should == exactum[:id]
+    end
+
+    it "should give correct id for Chemicum" do
+      Unicafe::Restaurant.name_to_id(chemicum[:name]).should == chemicum[:id]
+    end
+
+    it "should raise error when unknown name is given" do
+      expect{ Unicafe::Restaurant.name_to_id("This restaurant doesn't exist") }.to raise_error(Unicafe::Restaurant::NotFound)
+    end
+
+  end
+
 end
