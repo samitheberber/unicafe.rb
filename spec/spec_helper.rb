4e6c31eead42064a11b8a8abca7434091872fe13
Sami Saada <sami.saada@gmail.com>
2012-09-30 16:54:05 +0300

    example xml in use

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ require 'unicafe'
 
 FakeWeb.allow_net_connect = false
 
+FIXTURES = File.dirname(File.expand_path(__FILE__)) + "/fixtures"
+
+def example_menu_xml
+  File.read("#{FIXTURES}/example_menu.xml")
+end
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
--- spec/unicafe/lunch_spec.rb
+++ spec/unicafe/lunch_spec.rb
@@ -4,10 +4,10 @@ describe Unicafe::Lunch do
 
   let(:restaurant_id) {123}
   let(:lunches_mock) {[mock(Unicafe::Lunch)]}
+  let(:restaurant_data) {example_menu_xml}
 
   it "should fetch lunches according restaurant" do
-    restaurant_data = "foobar"
-    FakeWeb.register_uri(:get, "http://www.unicafe.fi/rss/fin/#{restaurant_id}/", body: restaurant_data)
+    FakeWeb.register_uri(:get, "http://www.unicafe.fi/rss/fin/#{restaurant_id}/", body: restaurant_data, :content_type => 'application/xml')
     Unicafe::Lunch.should_receive(:parse_data).with(restaurant_data).and_return(lunches_mock)
     Unicafe::Lunch.lunches_for_restaurant(restaurant_id).should == lunches_mock
   end
