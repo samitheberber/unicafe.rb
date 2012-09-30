96a72c6b0850167a6990f8bd94c5ad089f8d1211
Sami Saada <sami.saada@gmail.com>
2012-09-30 17:44:21 +0300

    data parsing

--- lib/unicafe/lunch.rb
+++ lib/unicafe/lunch.rb
@@ -3,8 +3,13 @@ module Unicafe
   class Lunch
 
     def self.lunches_for_restaurant id
-      content = Feedzirra::Feed.fetch_and_parse("http://www.unicafe.fi/rss/fin/#{id}/")
-      self.format_data content
+      content = Net::HTTP.get(URI.parse("http://www.unicafe.fi/rss/fin/#{id}/"))
+      self.parse_data content
+    end
+
+    def self.parse_data data
+      parsed_data = Feedzirra::Feed.parse data
+      self.format_data parsed_data
     end
 
   end
--- spec/unicafe/lunch_spec.rb
+++ spec/unicafe/lunch_spec.rb
@@ -8,9 +8,16 @@ describe Unicafe::Lunch do
   let(:menu_uri) {"http://www.unicafe.fi/rss/fin/#{restaurant_id}/"}
 
   it "should fetch lunches according restaurant" do
-    Feedzirra::Feed.should_receive(:fetch_and_parse).with(menu_uri).and_return(restaurant_data)
-    Unicafe::Lunch.should_receive(:format_data).with(restaurant_data).and_return(lunches_mock)
+    FakeWeb.register_uri(:get, menu_uri, body: restaurant_data)
+    Unicafe::Lunch.should_receive(:parse_data).with(restaurant_data).and_return(lunches_mock)
     Unicafe::Lunch.lunches_for_restaurant(restaurant_id).should == lunches_mock
+  end
+
+  it "should parse lunches from XML" do
+    parsed_data = mock(Feedzirra::Feed)
+    Feedzirra::Feed.should_receive(:parse).with(restaurant_data).and_return(parsed_data)
+    Unicafe::Lunch.should_receive(:format_data).with(parsed_data).and_return(lunches_mock)
+    Unicafe::Lunch.parse_data(restaurant_data).should == lunches_mock
   end
 
 end
