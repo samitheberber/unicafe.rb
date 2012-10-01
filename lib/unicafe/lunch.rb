88f2fabcd7553807f0223c3cd6754ebc894d6c64
Sami Saada <sami.saada@gmail.com>
2012-10-01 13:57:31 +0300

    correct requires

--- lib/unicafe/lunch.rb
+++ lib/unicafe/lunch.rb
@@ -1,9 +1,11 @@
 require 'feedzirra'
+require 'net/http'
+
 module Unicafe
   class Lunch
 
     def self.lunches_for_restaurant id
-      content = Net::HTTP.get(URI.parse("http://www.unicafe.fi/rss/fin/#{id}/"))
+      content = ::Net::HTTP.get(URI.parse("http://www.unicafe.fi/rss/fin/#{id}/"))
       self.parse_data content
     end
 
