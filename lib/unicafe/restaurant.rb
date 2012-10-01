d0339299b081635c782c0beec203420ec642e45f
Sami Saada <sami.saada@gmail.com>
2012-10-02 01:03:26 +0300

    locations to rest

--- lib/unicafe/restaurant.rb
+++ lib/unicafe/restaurant.rb
@@ -4,25 +4,24 @@ module Unicafe
   class Restaurant
 
     LIST_OF_RESTAURANTS = {
-      1 => {name: "Metsätalo"},
-      2 => {name: "Olivia"},
-      3 => {name: "Porthania"},
-      4 => {name: "Päärakennus"},
-      5 => {name: "Rotunda"},
-      6 => {name: "Topelias"},
-      7 => {name: "Valtiotiede"},
-      8 => {name: "Ylioppilasaukio"},
-      10 => {name: "Chemicum", latitude: 60.205108, longitude:24.963357},
-      11 => {name: "Exactum"},
+      1 => {name: "Metsätalo", latitude: "60.172577", longitude: "24.948878"},
+      2 => {name: "Olivia", latitude: "60.175077", longitude: "24.952979"},
+      3 => {name: "Porthania", latitude: "60.169878", longitude: "24.948669"},
+      4 => {name: "Päärakennus", latitude: "60.169178", longitude: "24.949297"},
+      5 => {name: "Rotunda", latitude: "60.170332", longitude: "24.950791"},
+      6 => {name: "Topelias", latitude: "60.171806", longitude: "24.95067"},
+      7 => {name: "Valtiotiede", latitude: "60.173897", longitude: "24.953095"},
+      8 => {name: "Ylioppilasaukio", latitude: "60.169092", longitude: "24.93992"},
+      10 => {name: "Chemicum", latitude: "60.205108", longitude: "24.963357"},
+      11 => {name: "Exactum", latitude: "60.20509", longitude: "24.961209"},
       12 => {name: "Physicum"},
-      13 => {name: "Meilahti"},
-      14 => {name: "Ruskeasuo"},
-      15 => {name: "Soc & kom"},
-      16 => {name: "Kookos"},
-      17 => {name: "Valdemar"},
-      18 => {name: "Biokeskus"},
-      19 => {name: "Korona"},
-      21 => {name: "Viikuna"}
+      13 => {name: "Meilahti", latitude: "60.190212", longitude: "24.908911"},
+      14 => {name: "Ruskeasuo", latitude: "60.206341", longitude: "24.895871"},
+      15 => {name: "Soc & kom", latitude: "60.173054", longitude: "24.95049"},
+      16 => {name: "Kookos", latitude: "60.181034", longitude: "24.958652"},
+      18 => {name: "Biokeskus", latitude: "60.226922", longitude: "25.013707"},
+      19 => {name: "Korona", latitude: "60.226922", longitude: "25.013707"},
+      21 => {name: "Viikuna", latitude: "60.23049", longitude: "25.020544"}
     }
 
     def initialize id
